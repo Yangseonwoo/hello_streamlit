4a06eca634e61e45f65801900ced964b63a623f4
Yangseonwoo <ysw3147@naver.com>
2024-11-25 17:04:54 +0900

    hello_streamlit(database)
    데이터베이스 hello_streamlit.py 제출합니다.

---
+++ hello_streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.write('Hello world!')
+
+import streamlit as st
+st.header('st.button')
+
+if st.button('say hello'):
+    st.write('why hello there')
+else:
+    st.write('goodbye')
